0bacdfd4d005a541ab9d1c0074ad01495f4111a8
TMoechel <t.moechel@softcreation.de>
2021-10-24 13:52:58 +0200

    Create restaurant list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "./App.css";
 
-const list = [
+const restaurantList = [
   {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
+    name: "Happy Elefant",
+    location: "Lodwar",
+    owner: "Peter Miller",
+    stars: 4,
     objectID: 0,
   },
   {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
+    name: "Late Tiger",
+    location: "Lodwar",
+    owner: "John Finch",
+    stars: 5,
+    objectID: 0,
   },
 ];
 
@@ -27,15 +25,16 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      {list.map(function (item) {
+      {restaurantList.map(function (item) {
         return (
           <div key={item.objectID} className="container">
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span>{item.location}</span>
+            <span>{item.name}</span>
+            <span>{item.stars}</span>
+            <span>{item.owner}</span>
           </div>
         );
       })}

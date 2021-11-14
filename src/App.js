c0b7d53d683e6f660ade0cf23d88b7d001d6ba1e
TMoechel <t.moechel@softcreation.de>
2021-11-14 15:01:51 +0100

    Remove redundant braces

--- src/App.js
+++ src/App.js
@@ -33,20 +33,18 @@ function App() {
 }
 
 function List() {
-  {
-    return list.map(function (item) {
-      return (
-        <div key={item.objectID} className="container">
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      );
-    });
-  }
+  return list.map(function (item) {
+    return (
+      <div key={item.objectID} className="container">
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
 }
 
 export default App;

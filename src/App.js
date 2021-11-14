9593bc4d88d0df94a858733e66e0eb8633ea2bd6
TMoechel <t.moechel@softcreation.de>
2021-11-14 14:29:53 +0100

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -27,20 +27,26 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      {list.map(function (item) {
-        return (
-          <div key={item.objectID} className="container">
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List />
     </div>
   );
+}
+
+function List() {
+  {
+    return list.map(function (item) {
+      return (
+        <div key={item.objectID} className="container">
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+    });
+  }
 }
 
 export default App;

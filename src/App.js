7284d2235ae745635d3d1d80083fd4cca0c40a20
TMoechel <t.moechel@softcreation.de>
2021-11-14 16:15:11 +0100

    Remove changes

--- src/App.js
+++ src/App.js
@@ -14,39 +14,32 @@ const restaurantList = [
     location: "Lodwar",
     owner: "John Finch",
     stars: 5,
-    objectID: 1,
+    objectID: 0,
   },
 ];
 
 function App() {
   return (
     <div>
-      <Header />
+      <h1>Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      <RestaurantList />
+      {restaurantList.map(function (item) {
+        return (
+          <div key={item.objectID} className="container">
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.location}</span>
+            <span>{item.name}</span>
+            <span>{item.stars}</span>
+            <span>{item.owner}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-function Header() {
-  return <h1>Lodwars Restaurants</h1>;
-}
-
-function RestaurantList() {
-  return restaurantList.map(function (item) {
-    return (
-      <div key={item.objectID} className="container">
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.location}</span>
-        <span>{item.name}</span>
-        <span>{item.stars}</span>
-        <span>{item.owner}</span>
-      </div>
-    );
-  });
-}
 export default App;

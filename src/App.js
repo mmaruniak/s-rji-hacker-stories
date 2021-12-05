44cce2039d06a2cb267b8f29bf201e76f4c6528a
TMoechel <t.moechel@softcreation.de>
2021-12-05 09:19:40 +0100

    Using HandlerFunction and Props

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
-  <div>
-    <h1>Hacker Stories</h1>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-    <hr />
-    <List />
-  </div>
-);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
 
-const List = () =>
-  list.map((item) => (
+  return (
+    <div>
+      <h1>Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
+
+const List = (props) =>
+  props.list.map((item) => (
     <div key={item.objectID} className="container">
       <span>
         <a href={item.url}>{item.title}</a>

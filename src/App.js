e39117f284da7f00ea396562bd654676973bccc5
TMoechel <t.moechel@softcreation.de>
2021-12-05 10:29:38 +0100

    Add a blur event

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  const handleTextChange = (event) => alert(event.target.value);
+  //const handleTextChange = (event) => alert(event.target.value);
+
+  const handleBlur = (event) =>
+    alert(`Do you want to save ${event.target.value}?`);
 
   return (
     <div>
       <h1>Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleTextChange} />
+      <input id="search" type="text" onBlur={handleBlur} />
+
       <hr />
       <Header />
     </div>

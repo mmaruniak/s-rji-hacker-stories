6f422aa9692b1e925aae99b973561134ecd8f41e
TMoechel <t.moechel@softcreation.de>
2021-12-12 15:02:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function greetLion(lionName) {
-  return `Hello ${lionName}`;
-}
-
 function App() {
+  const cityArray = ["London", "New York", "Berlin", "Wiesbaden"];
+  const [city1, city2] = cityArray;
+
   return (
     <div>
-      <h1>{greetLion("Mike")}</h1>
+      <h2>{city1}</h2>
+      <h2>{city2}</h2>
     </div>
   );
 }

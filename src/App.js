9d5630b236667506a517ccab993014d6db086e9b
TMoechel <t.moechel@softcreation.de>
2021-12-12 14:47:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 function App() {
   const [cityState, setCityState] = React.useState("London");
 
-  const handleButtonClick = () => {
-    //
+  const handleButtonClick = (event) => {
+    const newCity = "New York";
+    setCityState(newCity);
   };
 
   return (
     <div>
       <h1>Set my favorite city </h1>
+      <input type="button" onClick={handleButtonClick} value="Set City"></input>
       <h2>My favorite city is {cityState}</h2>
     </div>
   );

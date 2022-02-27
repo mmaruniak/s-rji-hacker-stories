eaffff5efb7c8f9de29b4796c18b95ef44846512
TMoechel <t.moechel@softcreation.de>
2022-02-27 09:52:12 +0100

    Update App.js
    Add Child City component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 function App() {
-  const [cityState, setCityState] = React.useState("London");
-
-  const handleButtonClick = () => {
-    //
-  };
+  const [cityState, setCityState] = React.useState("");
 
   return (
     <div>
       <h1>Set my favorite city </h1>
-      <h2>My favorite city is {cityState}</h2>
+      <City />
     </div>
   );
+}
+
+function City() {
+  return <h2> my favourite city is London</h2>;
 }
 
 export default App;

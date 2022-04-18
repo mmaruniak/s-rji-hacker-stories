7633d15e55a76e9db867d9afd90dcdf67532a155
TMoechel <t.moechel@softcreation.de>
2022-04-18 09:14:06 +0200

    refactor list component

--- src/App.js
+++ src/App.js
@@ -48,16 +48,17 @@ const Search = ({ search, onSearch }) => (
   </div>
 );
 
-const List = (props) =>
-  props.list.map((item) => (
-    <div key={item.objectID} className="container">
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const List = ({ list }) => list.map((item) => <Item item={item} />);
+
+const Item = ({ item }) => (
+  <div key={item.objectID} className="container">
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
 
 export default App;

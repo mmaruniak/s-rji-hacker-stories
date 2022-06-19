4056de99275b5be1403a2d2599ce0cea8609f519
TMoechel <t.moechel@softcreation.de>
2022-06-19 09:36:14 +0200

    building a form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  const [cityState, setCityState] = React.useState("");
+  const [personState, setPersonState] = React.useState("");
 
   return (
-    <div>
-      <h1>Set my favorite city </h1>
-      <City />
+    <div className="container">
+      <h1> Please enter your personal information </h1>
+      <PersonForm />
     </div>
   );
 }
 
-function City() {
-  return <h2> my favourite city is London</h2>;
+const onInputChange = () => {
+  alert("InputChanged");
+};
+
+function LabelInput({ label, value, onInputChange }) {
+  return (
+    <div className="labelInput">
+      <label htmlFor="textInput"> {label}</label>
+      <input
+        id="textInput"
+        type="text"
+        value={value}
+        onChange={onInputChange}
+      ></input>
+    </div>
+  );
+}
+
+function ButtonInput({ label }) {
+  return (
+    <div className="buttonInput">
+      <input type="button" value={label} />
+    </div>
+  );
+}
+
+function PersonForm({ label }) {
+  return (
+    <div>
+      <LabelInput label="First Name" value="" />
+      <LabelInput label="Last Name" value="" />
+      <LabelInput label="Date of Birth" value="" />
+      <LabelInput label="City of Birth" value="" />
+      <LabelInput label="Zip" value="" />
+      <LabelInput label="City" value="" />
+      <ButtonInput label="Submit" />
+    </div>
+  );
 }
 
 export default App;
